2ba8ba77afb263a4e5e2f490e0dc174a7f334f8f
Mert Kadir <mert.kadir9395@gmail.com>
2022-02-21 11:31:30 +0300

    Update Status.tsx

--- data/Status.tsx
+++ data/Status.tsx
@@ -72,10 +72,10 @@ const Status: React.FC = () => {
   
  if (loading === false && status?.discord_status === "online" && status?.listening_to_spotify === false )
  return ( 
- <Box display={"flex"} flexWrap={"nowrap"} float={"left"} padding={"5"}  marginTop={"5"} _hover={{ shadow: "lg" }}  backgroundColor={useColorModeValue("gray.100", "#1e2533")} rounded={"md"}>
+  <Tooltip hasArrow label="Discord Status" placement="top"><Box display={"flex"} flexWrap={"nowrap"} float={"left"} padding={"5"}  marginTop={"5"} _hover={{ shadow: "lg" }}  backgroundColor={useColorModeValue("gray.100", "#1e2533")} rounded={"md"}>
  <Stack backgroundColor={"green.500"} rounded={"full"} width={"4"} height={4} margin={"auto"} alignItems={"center"}></Stack>
  <Text textColor={useColorModeValue("black", "white")} fontSize={"md"}  textAlign={"center"} margin={"1"}> Online</Text>
-</Box> 
+</Box> </Tooltip>
 
  );
 
